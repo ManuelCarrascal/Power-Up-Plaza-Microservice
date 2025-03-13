3284e0461c0b203e605e6f4fbfd46c507a1cc334
Manuel Carrascal <tricdiart@gmail.com>
2025-03-13 18:36:46 -0500

    chore: setup basic microservice structure

---
+++ src/main/java/com/pragma/powerup/PlazaMicroserviceApp.java
@@ -0,0 +1,13 @@
+package com.pragma.powerup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlazaMicroserviceApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlazaMicroserviceApp.class, args);
+	}
+
+}
